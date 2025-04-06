8ef38da3726daca00493014e537044c43decf5d7
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-06 12:27:58 +0700

    update

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,11 @@
-export const Greet = () => {
+type GreetProps = {
+  name: string;
+};
+
+export const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h2>HELLO WORLD</h2>
+      <h2>HELLO WORLD {props.name}</h2>
     </div>
   );
 };
