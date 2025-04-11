652945440a7c1f208f19111e5c7e845c5ea4bc01
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-11 21:44:38 +0700

    useState hook

--- src/components/state/LoggedIn.tsx
+++ src/components/state/LoggedIn.tsx
@@ -1,14 +1,19 @@
-import { Button } from "../Button";
+import { useState } from "react";
 
 export const LoggedIn = () => {
-  const handleLogin = () => {};
-  const handleLogout = () => {};
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User is logged in / logged out</div>
+      <div>User is {isLoggedIn ? "Logged in" : "Logged out"}</div>
     </div>
   );
 };
