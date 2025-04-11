7df55453118426b7eee12e6de44b7bc5973341f6
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-11 21:33:28 +0700

    Props Types and Tips

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,13 +5,11 @@ type InputProps = {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Input = (props: InputProps) => {
+export const Input = ({ value, handleChange }: InputProps) => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event);
   };
-  return (
-    <input type="text" value={props.value} onChange={props.handleChange} />
-  );
+  return <input type="text" value={value} onChange={handleChange} />;
 };
 
 // component này cần 2 props giá trị đầu vào và trình xử lý onchange
