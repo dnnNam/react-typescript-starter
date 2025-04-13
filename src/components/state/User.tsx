5ae447cb4bd7efd397250fbbe80e78990722b9a9
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-13 21:49:11 +0700

    useState Future Value

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,14 +6,23 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState();
-  const handleLogin = () => {};
-  const handleLogout = () => {};
+  const [user, setUser] = useState<AuthUser | null>(null);
+  // phải định nghĩa object nếu không khi setUser typeScript sẽ báo lỗi
+  const handleLogin = () => {
+    setUser({
+      name: "hehe",
+      email: "hehe@gmail.com",
+    });
+  };
+  const handleLogout = () => {
+    setUser(null);
+  };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is</div>
+      <div>User name is{user?.name}</div>
+      <div>User email is{user?.email} </div>
     </div>
   );
 };
