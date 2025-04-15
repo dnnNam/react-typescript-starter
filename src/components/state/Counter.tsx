eca0ffb778bb1620b9129668db74222ed948efcf
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-15 22:04:53 +0700

    useReducer

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 const intialState = { count: 0 };
 
 function reducer(state, action) {
@@ -10,3 +12,18 @@ function reducer(state, action) {
       return state;
   }
 }
+
+export const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, intialState);
+  return (
+    <>
+      Count : {state.count}
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
+        Decrement 10
+      </button>
+    </>
+  );
+};
