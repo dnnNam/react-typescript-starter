4a24190cebe6c1993282fc28576e8fbde99da89a
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-16 19:55:57 +0700

    useReducer Strict Action Types

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,10 +4,16 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: "increment" | "decrement";
   payload: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
 const intialState = { count: 0 };
 
 function reducer(state: CounterState, action: CounterAction) {
@@ -16,6 +22,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload };
     case "decrement":
       return { count: state.count - action.payload };
+    case "reset":
+      return intialState;
     default:
       return state;
   }
@@ -32,6 +40,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </>
   );
 };
