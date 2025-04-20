c09c3a64af05ae54d8179ab58fd70cd158d01e73
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-20 21:53:04 +0700

    useRef

--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -4,7 +4,7 @@ export const MutableRef = () => {
   const [timer, setTimer] = useState(0);
   const intervalRef = useRef<number | null>(null);
   // vì setInternal có thể trả về id kiểu số
-
+  // thuộc tính current sẽ lưu giá trị của lần trước khi render
   const stopTimer = () => {
     if (intervalRef.current) window.clearInterval(intervalRef.current);
   };
