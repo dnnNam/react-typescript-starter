16f284cf5bf2b35b855d4e59b96d99f34ebd0bbf
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-20 21:27:39 +0700

    useRef

--- src/components/ref/DomRef.tsx
+++ src/components/ref/DomRef.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useRef } from "react";
 
 export const DomRef = () => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null!);
+  //Cái dấu ! sau null là non-null assertion operator của TypeScript.
+  // Nó báo với TypeScript rằng:
+  // Ê, tao biết giá trị này hiện tại là null,
+  // nhưng tao đảm bảo là sau này tao sẽ gán giá trị cho nó đúng cách,
+  //  và khi sử dụng nó, nó không null đâu, đừng báo lỗi với tao.”
   useEffect(() => {
-    inputRef.current.focus();
+    inputRef.current?.focus();
   }, []);
 
   return (
