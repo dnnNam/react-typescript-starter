ca973dd0f96573a02accf283ae3500beef8d521e
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-22 17:11:42 +0700

    Template Literals and Exclude

--- src/components/Templateliterals/Toast.tsx
+++ src/components/Templateliterals/Toast.tsx
@@ -1,3 +1,7 @@
+//  position props can be one of
+// "left-center" |"left-top" | "left-bottom" | "center" | "center-top"
+// "center-bottom" | "right-center" | "right-top" | "right-bottom"
+
 export const Toast = ({ position }) => {
   return <div>Toast Notification Position - {position}</div>;
 };
