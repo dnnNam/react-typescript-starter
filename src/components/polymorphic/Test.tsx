4d4060e81f92771b547c1e73159908546498bd0f
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-23 11:44:40 +0700

    Polymorphic Components

--- src/components/polymorphic/Test.tsx
+++ src/components/polymorphic/Test.tsx
@@ -1,13 +1,23 @@
-type TextProps = {
+import React from "react";
+
+type TextOwnProps<E extends React.ElementType> = {
   size?: "sm" | "md" | "lg";
   color?: "primary" | "secondary";
   children: React.ReactNode;
-  as?: React.ElementType;
+  as?: E;
 };
 
 // định nghĩa as để dễ kiểm soát các thành phần có trong component
 
-export const Text = ({ size, color, children, as }: TextProps) => {
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextOwnProps<E>>;
+
+export const Text = <E extends React.ElementType = "div">({
+  size,
+  color,
+  children,
+  as,
+}: TextProps<E>) => {
   const Component = as || "div";
   return (
     <Component className={`class-with-${size}-${color}`}>{children}</Component>
