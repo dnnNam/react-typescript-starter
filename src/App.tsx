b72e91a64f94db6174f6d1f9b844a2975b1a5ddc
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-23 11:36:53 +0700

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import { Toast } from "./components/Templateliterals/Toast";
 import { CustomButton } from "./components/html/Button";
+import { Text } from "./components/polymorphic/Test";
 
 // interface State {
 //   count: number;
@@ -132,9 +133,19 @@ function App() {
       {/* <Toast position="center" />
        */}
 
-      <CustomButton variant="primary" onClick={() => console.log("clicked")}>
+      {/* <CustomButton variant="primary" onClick={() => console.log("clicked")}>
         Primary Button
-      </CustomButton>
+      </CustomButton> */}
+
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
--- src/components/polymorphic/Test.tsx
+++ src/components/polymorphic/Test.tsx
@@ -2,8 +2,14 @@ type TextProps = {
   size?: "sm" | "md" | "lg";
   color?: "primary" | "secondary";
   children: React.ReactNode;
+  as?: React.ElementType;
 };
 
-export const Text = ({ size, color, children }: TextProps) => {
-  return <div className={`class-with-${size}-${color}`}>{children}</div>;
+// định nghĩa as để dễ kiểm soát các thành phần có trong component
+
+export const Text = ({ size, color, children, as }: TextProps) => {
+  const Component = as || "div";
+  return (
+    <Component className={`class-with-${size}-${color}`}>{children}</Component>
+  );
 };
